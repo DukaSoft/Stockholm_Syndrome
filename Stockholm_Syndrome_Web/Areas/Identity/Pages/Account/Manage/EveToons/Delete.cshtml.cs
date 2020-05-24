2b7789f00271838684bb2be45335618deebadcaa
Simon Kjærulff <dukasoft@klbn.dk>
2020-05-24 10:03:11 +0200

    Initial

---
+++ Stockholm_Syndrome_Web/Areas/Identity/Pages/Account/Manage/EveToons/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stockholm_Syndrome_Web.Data;
+
+namespace Stockholm_Syndrome_Web.Areas.Identity.Pages.Account.Manage.EveToons
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public EveCharacter EveCharacter { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            EveCharacter = await _context.EveCharacters.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (EveCharacter == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            EveCharacter = await _context.EveCharacters.FindAsync(id);
+
+            if (EveCharacter != null)
+            {
+                _context.EveCharacters.Remove(EveCharacter);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Stockholm_Syndrome_Web/Areas/Identity/Pages/Account/Manage/EveToons/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stockholm_Syndrome_Web.Data;
+
+namespace Stockholm_Syndrome_Web.Pages.YourUser
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EveCharacter> EveCharacter { get; set; }
+        public string State { get; set; }
+
+        public async Task OnGetAsync(string state = null)
+        {
+            State = state;
+            EveCharacter = await _context.EveCharacters.Where(c => c.User.UserName == User.Identity.Name).ToListAsync();
+        }
+    }
+}
