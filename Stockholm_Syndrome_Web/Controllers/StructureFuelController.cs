796a0b87c0e03af1f0eb5cfb2ada77b041a8ff68
Simon Kjærulff <dukasoft@klbn.dk>
2020-06-06 17:12:49 +0200

    Api For Structure Fuel #131

---
+++ Stockholm_Syndrome_Web/Controllers/StructureFuelController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stockholm_Syndrome_Web.Data;
+using Stockholm_Syndrome_Web.Models;
+
+namespace Stockholm_Syndrome_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StructureFuelController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StructureFuelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/StructureFuel
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StructureFuelModel>>> GetStructures(string token)
+        {
+            if (token != "somesecurityhere!")
+			{
+				return NotFound();
+			}
+
+			List<StructureFuelModel> structureFuel = new List<StructureFuelModel>();
+
+            var allStructures = await _context.Structures.ToListAsync();
+
+            foreach(var structure in allStructures)
+			{
+                DateTime? date;
+                try
+                {
+                    date = DateTime.Parse(structure.FuelExpires, new System.Globalization.CultureInfo("en-US"));
+                }
+                catch
+                {
+                    // Its ok if this fails
+                    date = null;
+                }
+
+                StructureFuelModel fm = new StructureFuelModel();
+
+                fm.Name = structure.StructureName;
+                if(date != null)
+				{
+                    fm.Expire = (DateTime)date;
+				}
+
+                structureFuel.Add(fm);
+			}
+
+            return structureFuel;
+        }
+    }
+}
---
+++ Stockholm_Syndrome_Web/Models/StructureFuelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stockholm_Syndrome_Web.Models
+{
+	public class StructureFuelModel
+	{
+		public string Name { get; set; }
+		public DateTime Expire { get; set; }
+
+	}
+}
