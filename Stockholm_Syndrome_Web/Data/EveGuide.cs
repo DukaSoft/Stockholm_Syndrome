e2208c4d4620cd1ba46f0f264c7988a2ce706e7d
Simon Kjærulff <dukasoft@klbn.dk>
2020-09-10 00:24:53 +0200

    Source display name change

--- Stockholm_Syndrome_Web/Data/EveGuide.cs
+++ Stockholm_Syndrome_Web/Data/EveGuide.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,8 @@ namespace Stockholm_Syndrome_Web.Data
 		public int Id { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
+
+		[Display(Name ="Source")]
 		public string Src { get; set; }
 
 	}
