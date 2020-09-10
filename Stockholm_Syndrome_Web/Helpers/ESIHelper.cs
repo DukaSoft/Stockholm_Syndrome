711e459813546072444271dd2e3471d4853a337b
Simon Kjærulff <dukasoft@klbn.dk>
2020-09-10 11:25:08 +0200

    ESI Scope moved to Variable

--- Stockholm_Syndrome_Web/Helpers/ESIHelper.cs
+++ Stockholm_Syndrome_Web/Helpers/ESIHelper.cs
@@ -14,6 +14,13 @@ namespace Stockholm_Syndrome_Web.Helpers
 {
 	public class ESIHelper
 	{
+
+		public static string CurrentESIScopeForUsers =
+			"publicData esi-skills.read_skills.v1 esi-ui.write_waypoint.v1 esi-fittings.read_fittings.v1 esi-fittings.write_fittings.v1";
+
+		public static string CurrentESIScopeForCorp =
+			"esi-universe.read_structures.v1 esi-corporations.read_structures.v1 esi-industry.read_corporation_mining.v1 esi-characters.read_notifications.v1";
+
 		public class ESITokenRefresh
 		{
 			public string grant_type { get; set; }
