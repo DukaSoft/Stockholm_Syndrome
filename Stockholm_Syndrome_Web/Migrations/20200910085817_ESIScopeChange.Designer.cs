fbb20ab80a47c8aaced6dedb7506c972e2bba9fc
Simon Kjærulff <dukasoft@klbn.dk>
2020-09-10 11:24:27 +0200

    #139

--- Stockholm_Syndrome_Web/Migrations/20200910085817_ESIScopeChange.Designer.cs
+++ Stockholm_Syndrome_Web/Migrations/20200910085817_ESIScopeChange.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Stockholm_Syndrome_Web.Data;
 
 namespace Stockholm_Syndrome_Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200910085817_ESIScopeChange")]
+    partial class ESIScopeChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Stockholm_Syndrome_Web/Migrations/20200910085817_ESIScopeChange.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stockholm_Syndrome_Web.Migrations
+{
+    public partial class ESIScopeChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ESIScope",
+                table: "EveCharacters",
+                maxLength: 2000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ESIScope",
+                table: "EveCharacters");
+        }
+    }
+}
