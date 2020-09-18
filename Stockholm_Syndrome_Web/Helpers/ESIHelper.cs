eec994b35582453ed8555b70eeaa467be3cceaa1
Simon Kjærulff <dukasoft@klbn.dk>
2020-09-18 17:36:02 +0200

    ESI Scope Change

--- Stockholm_Syndrome_Web/Helpers/ESIHelper.cs
+++ Stockholm_Syndrome_Web/Helpers/ESIHelper.cs
@@ -16,7 +16,7 @@ namespace Stockholm_Syndrome_Web.Helpers
 	{
 
 		public static string CurrentESIScopeForUsers =
-			"publicData esi-skills.read_skills.v1 esi-ui.write_waypoint.v1 esi-fittings.read_fittings.v1 esi-fittings.write_fittings.v1";
+			"publicData esi-skills.read_skills.v1 esi-ui.write_waypoint.v1 esi-fittings.read_fittings.v1 esi-fittings.write_fittings.v1 esi-fleets.read_fleet.v1";
 
 		public static string CurrentESIScopeForCorp =
 			"esi-universe.read_structures.v1 esi-corporations.read_structures.v1 esi-industry.read_corporation_mining.v1 esi-characters.read_notifications.v1";
